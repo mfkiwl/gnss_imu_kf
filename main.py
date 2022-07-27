de4b36f2636cc96cc7d49747c83e700c8b2a737b
Ignat Penshin <61110400+ignatpenshin@users.noreply.github.com>
2022-07-27 19:11:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ if __name__ == "__main__":
     x_true = []
     y_true = []
 
+    x_interp = []
+    y_interp = []
+
     x_calc, y_calc = [], []
+
+    x_imu_pos = []
+    y_imu_pos = []
 
     try: 
         while True:
@@ -32,23 +38,31 @@ if __name__ == "__main__":
                     x_calc.append(gnss_predict['coords'][0])
                     y_calc.append(gnss_predict['coords'][1])
 
+                    x_interp.append(gnss_predict['coords'][0])
+                    y_interp.append(gnss_predict['coords'][1])
+
                 x_true.append(gnss_predict['coords'][0])
                 y_true.append(gnss_predict['coords'][1])
 
             else:
                 x_calc.append(x_calc[-1] + gnss_predict['velocity']*sin(radians(gnss_predict['heading']))*dt)
                 y_calc.append(y_calc[-1] + gnss_predict['velocity']*cos(radians(gnss_predict['heading']))*dt)
+            
+            if len(x_true) + len(y_true) >= 4:
+                x_interp.append(x_interp[-1] + (x_true[-1] - x_true[-2])*dt)
+                y_interp.append(y_interp[-1] + (y_true[-1] - y_true[-2])*dt)
 
             gnss, imu = reader.next()
+            #print(x_calc[-1] - x_true[-1], y_calc[-1] - y_true[-1])
+            
 
             # IMU BLOCK
 
 
     except StopIteration:
         plt.plot(x_true, y_true)
-        plt.title('True coords')
-
         plt.plot(x_calc, y_calc)
-        plt.title('Calc coords (V, Head)')
+        plt.plot(x_interp, y_interp)
+
 
         plt.show()
